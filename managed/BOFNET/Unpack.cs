11c3e9e78b9b6a69c3f6befb3b7ae4828b64b65c
CCob <CCob@users.noreply.github.com>
2020-11-06 21:49:37 +0000

    Exposes bofnet_execute_raw function
    This allows BOF.NET classes to implent Go(byte[]) prototype and use the Unpack class to unpack bytes packed with bof_pack

---
+++ managed/BOFNET/Unpack.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BOFNET {
+    /// <summary>
+    /// Unpacks a byte[] than has been packed with aggressor script's bof_pack function, note that z is not supported
+    /// right now, only Z which should work in most cases.
+    /// </summary>
+    public class Unpack {
+
+        public List<object> Values { get; private set; } = new List<object>();
+
+        public Unpack(string format, byte[] data) {            
+
+            BinaryReader br = new BinaryReader(new MemoryStream(data), Encoding.Unicode);
+            
+            foreach(char code in format) {
+
+                switch (code) {
+                    case 'Z':
+                        int lenRaw = br.ReadInt32BE();
+                        StringBuilder str = new StringBuilder();
+                        char ch;
+
+                        while ((ch = br.ReadChar()) != 0) {
+                            str.Append(ch);
+                        }
+
+                        Values.Add(str.ToString());
+                        break;
+                                                                                   
+                    case 'i':
+                        Values.Add(br.ReadInt32BE());
+                        break;
+
+                    case 's':
+                        Values.Add(br.ReadInt16());
+                        break;
+
+                    case 'b':
+                        Values.Add(br.ReadBytes(br.ReadInt32BE()));
+                        break;
+
+                    default:
+                        throw new ArgumentException($"Invalid type {code} encountered in format string. Currently only type Z,i,s,b are supported");
+                }
+            }
+        }
+    }
+}
---
+++ managed/BOFNET/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BOFNET {
+
+    // Base on code from here: https://stackoverflow.com/questions/8620885/c-sharp-binary-reader-in-big-endian
+    public static class Utils {
+        // Note this MODIFIES THE GIVEN ARRAY then returns a reference to the modified array.
+        public static byte[] Reverse(this byte[] b) {
+            Array.Reverse(b);
+            return b;
+        }
+
+        public static UInt16 ReadUInt16BE(this BinaryReader binRdr) {
+            return BitConverter.ToUInt16(binRdr.ReadBytes(sizeof(UInt16)).Reverse(), 0);
+        }
+
+        public static Int16 ReadInt16BE(this BinaryReader binRdr) {
+            return BitConverter.ToInt16(binRdr.ReadBytes(sizeof(Int16)).Reverse(), 0);
+        }
+
+        public static UInt32 ReadUInt32BE(this BinaryReader binRdr) {
+            return BitConverter.ToUInt32(binRdr.ReadBytes(sizeof(UInt32)).Reverse(), 0);
+        }
+
+        public static Int32 ReadInt32BE(this BinaryReader binRdr) {
+            return BitConverter.ToInt32(binRdr.ReadBytes(sizeof(Int32)).Reverse(), 0);
+        }
+    }
+ }
