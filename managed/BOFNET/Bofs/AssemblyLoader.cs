806e9790f74e9a7670f152941ffda93268641303
CCob <CCob@users.noreply.github.com>
2020-12-28 12:08:11 +0000

    Updated AssemblyLoader to support chunked loading for large assemblies

--- managed/BOFNET/Bofs/AssemblyLoader.cs
+++ managed/BOFNET/Bofs/AssemblyLoader.cs
@@ -1,15 +1,25 @@
-﻿using System;
+﻿using BOFNET;
+using BOFNET.Bofs;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Reflection;
-using System.Text;
 
 namespace BOFNET.Bofs {
+
+    /// <summary>
+    /// AssemblyLoader that supports feeding chunks of assembly data for large binaries (+1MB).  
+    /// </summary>
     public class AssemblyLoader : BeaconObject {
+
+        private static Dictionary<string, MemoryStream> activeAssemblyLoads = new Dictionary<string, MemoryStream>();
+
+        BeaconApi api;
+
         public AssemblyLoader(BeaconApi api) : base(api) {
+            this.api = api;
         }
 
-        public override void Go(byte[] assemblyData) {
+        private void LoadAssembly(byte[] assemblyData) {
             Assembly assembly = Runtime.LoadAssembly(assemblyData);
             BeaconConsole.WriteLine($"[+] Loaded assembly {assembly.FullName} successfully");
 
@@ -19,6 +29,31 @@ namespace BOFNET.Bofs {
                 var attachMethod = assemblyLoaderType?.GetMethod("Attach", BindingFlags.Static | BindingFlags.Public);
                 attachMethod?.Invoke(null, new object[] { });
             }
+        }
+
+        public override void Go(byte[] chunk) {
+
+            Unpack unpack = new Unpack("Zib", chunk);
+
+            string assemblyName = (string)unpack.Values[0];
+            int chunkSize = (int)unpack.Values[1];
+            byte[] assemblyChunk = (byte[])unpack.Values[2];
+            MemoryStream assemblyData;
+
+            if (!activeAssemblyLoads.ContainsKey(assemblyName)) {
+                BeaconConsole.WriteLine($"[+] Setting up new loader with unique id {assemblyName}");
+                assemblyData = new MemoryStream();                
+                activeAssemblyLoads[assemblyName] = assemblyData;
+            } else {                
+                assemblyData = activeAssemblyLoads[assemblyName];
+            }
+
+            assemblyData.Write(assemblyChunk, 0, assemblyChunk.Length);
+
+            if (assemblyChunk.Length < chunkSize) {   
+                LoadAssembly(assemblyData.ToArray());
+                activeAssemblyLoads.Remove(assemblyName);                
+            } 
         }
     }
 }
